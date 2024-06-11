63df3170a3040e35876c2408fa5b8a9fa146d2e9
jianlikepc2100 <48867052+clarklikepc2100@users.noreply.github.com>
2024-06-11 15:47:20 -0400

    up

---
+++ TU-101-llama2-TU-GPUserver-2204-py38/cmd-txt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+torchrun --nproc_per_node 1 example_text_completion.py \
+    --ckpt_dir llama-2-7b/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 128 --max_batch_size 4 >> out-txt.txt 
